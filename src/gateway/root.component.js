6e5444f1cf214559df4116aefd701627a1c8077f
Renan Inacio <renaninacio.13@gmail.com>
2020-10-11 19:36:11 -0300

    Create a gateway app

---
+++ src/gateway/root.component.js
@@ -0,0 +1,47 @@
+import React from "react"
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import './style.css';
+
+const App = () => {
+  return (
+    <div className="gateway">
+
+      <AppBar position="static" className="gateway-header">
+        <Toolbar>
+          <Typography variant="h6">
+            MELI Tech Day - Microfrontends demo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container>
+        <Grid container justify="center">
+          <Grid md={4} sm={6} xs={12} justify="center" className="gateway-search">
+            <TextField label="Repo" size="medium" fullWidth />
+            <Button variant="contained">Search</Button>
+          </Grid>
+        </Grid>
+      </Container>
+
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <div id="react-app"></div>
+          </Grid>
+          <Grid item xs={8}>
+            <div id="vue-app"></div>
+          </Grid>
+        </Grid>
+      </Container>
+      
+    </div>
+  )
+}
+
+export default App
--- src/react/root.component.js
+++ src/react/root.component.js
@@ -1,5 +1,19 @@
 import React from "react"
 
-const App = () => <h1>Hello from React</h1>
+const App = () => {
+  window.addEventListener('hashchange', evt => {
+    const repo = window.location.hash.split('#')[1]
+
+    loadRepo({ repo })
+  })
+
+  const loadRepo = ({ repo }) => {
+    console.log(`From react, load te repo ${repo}`)
+  }
+
+  return (
+    <h1>Hello from React</h1>
+  )
+}
 
 export default App
