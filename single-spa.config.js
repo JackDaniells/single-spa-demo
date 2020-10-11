055287a5eac438c0d8b664085c59245de9c4dea3
Daniel Kock <daniel.kock@mercadolivre.com>
2020-10-10 22:09:52 -0300

    remove unused components

--- single-spa.config.js
+++ single-spa.config.js
@@ -1,16 +1,15 @@
 import { registerApplication, start } from 'single-spa'
-import 'babel-polyfill';
 
+registerApplication(
+  'vue', 
+  () => import('./src/vue/vue.app.js'),
+  () => location.pathname === "/react" ? false : true
+);
 
-
-function pathPrefix(prefix) {
-    true
-    // return location.pathname === prefix
-}
-registerApplication('navbar', () => import('./src/navbar/navbar.app.js'),() => true);
-
-registerApplication('vue', () => import('./src/vue/vue.app.js'),() => pathPrefix("/vue"));
- 
-// registerApplication('react', () => import('./src/react/main.app.js'),() =>  pathPrefix("/react"));
+registerApplication(
+  'react',
+  () => import('./src/react/main.app.js'),
+  () => location.pathname === "/vue"  ? false : true
+);
 
 start();
